48caa4b8180369d87b592943276117365f925913
Sumreed <932374224@qq.com>
2022-08-08 19:18:59 +0800

    date2022.08.08 zxw 信号量部分代码

--- face_time.h
+++ face_time.h
@@ -6,10 +6,21 @@
 typedef INT8U clockid_t;	/* 时钟标识				*/
 typedef INT32U clock_t;		/* 时钟跳数类型			*/
 // typedef INT32U time_t;		/* 秒为单位的时间			*/
+typedef INT32U timer_t;      /* 用来存储计时器ID      */
+
+
 struct timespec {
 	time_t  tv_sec;			/* 时间的秒数			*/		
 	long    tv_nsec;		/* 时间的纳秒数			*/
 };
+
+
+
+struct itimerspec{
+	struct timespec it_interval;
+	struct timespec it_value;
+};
+
 
 int clock_getres(clockid_t, struct timespec *);
 int clock_gettime(clockid_t, struct timespec *);
@@ -17,3 +28,12 @@ int clock_nanosleep(clockid_t, int, const struct timespec *, struct timespec *);
 int clock_settime(clockid_t, const struct timespec *);
 int nanosleep(const struct timespec *, struct timespec *);
 unsigned int sleep(unsigned int seconds);
+
+/*---计时器---*/
+int timer_create(clockid_t clockid, struct sigevent *evp, timer_t * timerid);
+int timer_delete(timer_t timerid);
+int timer_settime(timer_t timerid, int flags,
+	const struct itimerspec *value,
+struct itimerspec *ovalue);  /*这里曾删除restrict关键字（2022.08.02）*/
+int timer_gettime(timer_t timerid, struct itimerspec *value);
+int timer_getoverrun(timer_t timerid);
